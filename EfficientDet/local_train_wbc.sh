2fca99ec322f0bcbdff04caad1806c7bf5baa98c
Shenghua He <sh38@anastasio3.bioen.illinois.edu>
2021-02-28 13:27:19 -0600

    shh

--- EfficientDet/local_train_wbc.sh
+++ EfficientDet/local_train_wbc.sh
@@ -13,3 +13,8 @@ python3 train.py --snapshot imagenet --phi 4 --gpu 3 --snapshot-path ../../resul
 python3 train.py --snapshot imagenet --phi 5 --gpu 4 --snapshot-path ../../results/det5 --random-transform --compute-val-loss --freeze-backbone --batch-size 2 --steps 1000 csv ../../datasets/wbc_1024x1024/train.csv ../../datasets/wbc_1024x1024/class.csv --val-annotations-path ../../datasets/wbc_1024x1024/valid.csv
 
 python3 train.py --snapshot imagenet --phi 6 --gpu 1 --snapshot-path ../../results/det6 --random-transform --compute-val-loss --freeze-backbone --batch-size 2 --steps 1000 csv ../../datasets/wbc_1024x1024/train.csv ../../datasets/wbc_1024x1024/class.csv --val-annotations-path ../../datasets/wbc_1024x1024/valid.csv
+
+# Feb 27, 2021
+python3 train.py --snapshot imagenet --phi 1 --gpu 0 --snapshot-path ../../results/det1 --random-transform --compute-val-loss --freeze-backbone --batch-size 32 --steps 1000 csv ../../datasets/wbc2_1024x1024/train.csv ../../datasets/wbc2_1024x1024/class.csv --val-annotations-path ../../datasets/wbc2_1024x1024/valid.csv
+
+python3 train.py --snapshot imagenet --phi 4 --gpu 2 --snapshot-path ../../results/wbc2/det4 --random-transform --compute-val-loss --freeze-backbone --batch-size 4 --steps 1000 csv ../../datasets/wbc2_1024x1024/train.csv ../../datasets/wbc2_1024x1024/class.csv --val-annotations-path ../../datasets/wbc2_1024x1024/valid.csv
