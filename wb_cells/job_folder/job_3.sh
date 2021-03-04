e59078c77c187ca0c995bc604e208d1d12163346
shenghh <shenghh2015@gmail.com>
2021-03-04 12:07:05 -0600

    shh

--- wb_cells/job_folder/job_3.sh
+++ wb_cells/job_folder/job_3.sh
@@ -1,1 +1,1 @@
-python train_mask.py --gpu 3 --net_type Unet --backbone efficientnetb6 --epoch 2400 --dim 1024 --batch_size 1 --lr 1e-4 --dataset wbc3_1024x1024
+python train_mask2.py --gpu 3 --net_type Unet --backbone efficientnetb6 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc3_1024x1024
