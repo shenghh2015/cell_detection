7c8287d65a1c9c24a6b2b56e157ee28f38a197f4
shenghh <shenghh2015@gmail.com>
2021-03-04 11:44:03 -0600

    shh

--- wb_cells/multi_GPU.sh
+++ wb_cells/multi_GPU.sh
@@ -4,7 +4,7 @@
 # JOB: python train_mask.py --gpu 2 --net_type Unet --backbone efficientnetb6 --epoch 2400 --dim 1024 --batch_size 1 --lr 1e-4 --dataset wbc2_1024x1024
 # JOB: python train_mask.py --gpu 3 --net_type Unet --backbone efficientnetb6 --epoch 2400 --dim 1024 --batch_size 1 --lr 1e-4 --dataset wbc3_1024x1024
 
-JOB: python train_mask2.py --gpu 0 --net_type Unet --backbone efficientnetb7 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc2_1024x1024
-JOB: python train_mask2.py --gpu 1 --net_type Unet --backbone efficientnetb7 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc3_1024x1024
-JOB: python train_mask2.py --gpu 2 --net_type Unet --backbone efficientnetb6 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc2_1024x1024
-JOB: python train_mask2.py --gpu 3 --net_type Unet --backbone efficientnetb6 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc3_1024x1024
+JOB: python train_mask2.py --gpu 0 --net_type Unet --backbone efficientnetb5 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc2_1024x1024
+JOB: python train_mask2.py --gpu 1 --net_type Unet --backbone efficientnetb5 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc3_1024x1024
+JOB: python train_mask2.py --gpu 2 --net_type Unet --backbone efficientnetb4 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc2_1024x1024
+JOB: python train_mask2.py --gpu 3 --net_type Unet --backbone efficientnetb4 --epoch 2400 --dim 1024 --batch_size 2 --lr 1e-4 --dataset wbc3_1024x1024
