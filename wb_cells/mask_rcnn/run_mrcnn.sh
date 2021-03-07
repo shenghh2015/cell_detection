2b67767e9c751e14024a8651e6b10dfda17b0aa1
Shenghua He <sh38@compton.bioen.illinois.edu>
2021-03-07 12:17:18 -0600

    shh

--- wb_cells/mask_rcnn/run_mrcnn.sh
+++ wb_cells/mask_rcnn/run_mrcnn.sh
@@ -7,3 +7,7 @@ python3 wbc.py train --dataset=./dataset/data --subset=train --weights=imagenet 
 # Feb 25, 2021
 python3 wbc.py train --dataset=./dataset/data --subset=train --weights=coco --gpu 2,3 --backbone 'resnet50'
 python3 wbc.py train --dataset=./dataset/data --subset=train --weights=coco --gpu 3,7 --backbone 'resnet101'
+
+# Feb 28, 2021
+python3 wbc2.py train --dataset=./dataset2/data --subset=train --weights=coco --gpu 0,2 --backbone 'resnet50'
+python3 wbc2.py train --dataset=./dataset2/data --subset=train --weights=coco --gpu 0,1 --backbone 'resnet101'
