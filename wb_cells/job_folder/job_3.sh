4ed7126f7f57b8d7952d6f952193102dab08a21b
shenghh <shenghh2015@gmail.com>
2021-03-08 02:18:04 -0600

    shh

--- wb_cells/job_folder/job_3.sh
+++ wb_cells/job_folder/job_3.sh
@@ -1,1 +1,1 @@
-python train_crop_mask.py --gpu 3 --net_type Unet --backbone efficientnetb6 --epoch 4800 --dim 384 --dataset wbc3_1024x1024 --loss focal --batch 8 --lr 1e-4 --focal_weight 4 --bk 1 --rot 20
+python train_crop_mask.py --gpu 3 --net_type Unet --backbone efficientnetb7 --epoch 4800 --dim 384 --dataset wbc3_1024x1024 --loss focal+dice --batch 8 --lr 1e-4 --focal_weight 4 --bk 1 --rot 25
