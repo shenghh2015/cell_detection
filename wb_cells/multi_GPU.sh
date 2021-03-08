3b03f40ec2cdd878ea4cb63f0889eb89704d88d6
shenghh <shenghh2015@gmail.com>
2021-03-07 21:31:31 -0600

    shh

--- wb_cells/multi_GPU.sh
+++ wb_cells/multi_GPU.sh
@@ -17,5 +17,5 @@
 
 JOB: python train_crop_mask.py --gpu 0 --net_type Unet --backbone efficientnetb7 --epoch 4800 --dim 384 --dataset wbc_1024x1024 --loss focal --batch 8 --lr 1e-4 --focal_weight 4 --bk 1 --rot 20
 JOB: python train_crop_mask.py --gpu 1 --net_type Unet --backbone efficientnetb7 --epoch 4800 --dim 384 --dataset wbc3_1024x1024 --loss focal  --batch 8 --lr 1e-4 --focal_weight 4 --bk 1 --rot 20
-JOB: python train_crop_mask.py --gpu 2 --net_type Unet --backbone efficientnetb6 --epoch 4800 --dim 384 --dataset wbc2_1024x1024 --loss focal --batch 8 --lr 1e-4 --focal_weight 4 --bk 1 --rot 20
+JOB: python train_crop_mask.py --gpu 2 --net_type Unet --backbone efficientnetb6 --epoch 4800 --dim 384 --dataset wbc_1024x1024 --loss focal --batch 8 --lr 1e-4 --focal_weight 4 --bk 1 --rot 20
 JOB: python train_crop_mask.py --gpu 3 --net_type Unet --backbone efficientnetb6 --epoch 4800 --dim 384 --dataset wbc3_1024x1024 --loss focal --batch 8 --lr 1e-4 --focal_weight 4 --bk 1 --rot 20
