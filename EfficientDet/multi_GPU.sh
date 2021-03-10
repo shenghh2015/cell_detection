4760e4e8de5a23638d0879c6dc1a27ec46e2b939
shenghh <shenghh2015@gmail.com>
2021-03-10 01:42:20 -0600

    shh

--- EfficientDet/multi_GPU.sh
+++ EfficientDet/multi_GPU.sh
@@ -46,6 +46,6 @@
 
 # Mar 9, 2021
 JOB: python3 train_wbc.py --docker --snapshot imagenet --phi 1 --gpu 0 --batch-size 10 --epoch 400 --steps 100 --dataset wbc4_1024x1024 --cls 4 --valid False
-JOB: python3 train_wbc.py --docker --snapshot imagenet --phi 1 --gpu 1 --batch-size 10 --epoch 400 --steps 100 --dataset wbc4_1024x1024 --cls 4 --valid True
-JOB: python3 train_wbc.py --docker --snapshot imagenet --phi 1 --gpu 2 --batch-size 10 --epoch 400 --steps 100 --dataset wbc4_1024x1024 --cls 5 --valid False
+JOB: python3 train_wbc.py --docker --snapshot imagenet --phi 1 --gpu 1 --batch-size 10 --epoch 400 --steps 100 --dataset wbc4_1024x1024 --cls 5 --valid False
+JOB: python3 train_wbc.py --docker --snapshot imagenet --phi 1 --gpu 2 --batch-size 10 --epoch 400 --steps 100 --dataset wbc4_1024x1024 --cls 4 --valid True
 JOB: python3 train_wbc.py --docker --snapshot imagenet --phi 1 --gpu 3 --batch-size 10 --epoch 400 --steps 100 --dataset wbc4_1024x1024 --cls 5 --valid True
