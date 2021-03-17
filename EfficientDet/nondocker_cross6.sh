f59a98005d885e3961b782c1f4321d6e211db505
shenghh <shenghh2015@gmail.com>
2021-03-17 01:26:47 -0500

    shh

--- EfficientDet/nondocker_cross6.sh
+++ EfficientDet/nondocker_cross6.sh
@@ -1,4 +1,3 @@
 # Mar 17, 2021
-python3 train_wbc_cv.py --snapshot imagenet --dataset wbc4_1024x1024 --cls 4 --phi 0 --gpu 2 --batch-size 8 --steps 100 --epoch 200 --valid False --cross 1
-python3 train_wbc_cv.py --snapshot imagenet --dataset wbc4_1024x1024 --cls 4 --phi 0 --gpu 2 --batch-size 8 --steps 100 --epoch 200 --valid False --cross 2
-python3 train_wbc_cv.py --snapshot imagenet --dataset wbc4_1024x1024 --cls 4 --phi 0 --gpu 2 --batch-size 8 --steps 100 --epoch 200 --valid False --cross 3
+python3 train_wbc_cv.py --snapshot imagenet --dataset wbc2_1024x1024 --cls 5 --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --valid False --cross 4
+python3 train_wbc_cv.py --snapshot imagenet --dataset wbc2_1024x1024 --cls 5 --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --valid False --cross 5
