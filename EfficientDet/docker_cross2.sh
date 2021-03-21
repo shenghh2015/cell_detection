b03aea6a8e34a98a3a363000dbf9e820bd398830
shenghh <shenghh2015@gmail.com>
2021-03-20 20:10:05 -0500

    shh

--- EfficientDet/docker_cross2.sh
+++ EfficientDet/docker_cross2.sh
@@ -11,8 +11,8 @@
 # python3 train_wbc_cv.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --cls 4 --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --valid False --cross 4
 
 # Mar 20, 2021
-python3 train_wbc_cv.py --docker --snapshot imagenet --dataset wbc_1024x1024 --cls 5 --phi 1 --gpu 1 --batch-size 4 --steps 100 --epoch 200 --valid False --cross 1
-python3 train_wbc_cv.py --docker --snapshot imagenet --dataset wbc_1024x1024 --cls 5 --phi 1 --gpu 1 --batch-size 4 --steps 100 --epoch 200 --valid False --cross 2
+# python3 train_wbc_cv.py --docker --snapshot imagenet --dataset wbc_1024x1024 --cls 5 --phi 1 --gpu 1 --batch-size 4 --steps 100 --epoch 200 --valid False --cross 1
+# python3 train_wbc_cv.py --docker --snapshot imagenet --dataset wbc_1024x1024 --cls 5 --phi 1 --gpu 1 --batch-size 4 --steps 100 --epoch 200 --valid False --cross 2
 python3 train_wbc_cv.py --docker --snapshot imagenet --dataset wbc_1024x1024 --cls 5 --phi 1 --gpu 1 --batch-size 4 --steps 100 --epoch 200 --valid False --cross 3
 python3 train_wbc_cv.py --docker --snapshot imagenet --dataset wbc_1024x1024 --cls 5 --phi 1 --gpu 1 --batch-size 4 --steps 100 --epoch 200 --valid False --cross 4
 python3 train_wbc_cv.py --docker --snapshot imagenet --dataset wbc_1024x1024 --cls 5 --phi 1 --gpu 1 --batch-size 4 --steps 100 --epoch 200 --valid False --cross 5
