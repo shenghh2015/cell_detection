f81623de4dba820a1e709eb34a02fabdf576ac92
shenghh2015 <shenghh2015@gmail.com>
2021-07-07 01:54:39 -0500

    shh

--- EfficientDet/predator2.sh
+++ EfficientDet/predator2.sh
@@ -2,4 +2,4 @@
 # python3 train_wbc.py --docker --snapshot /data/coco/efficientdet-d0.h5 --dataset wbc_1024x1024 --phi 0 --gpu 0 --batch-size 8 --steps 100 --epoch 200 --weighted-bifpn
 python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc5_1024x1024 --valid False --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 3
 python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc5_1024x1024 --valid False --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 4
-python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc4_1024x1024 --valid False --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 0
+python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --valid False --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 0
--- EfficientDet/predator3.sh
+++ EfficientDet/predator3.sh
@@ -1,2 +1,6 @@
-python3 train_wbc.py --docker --snapshot imagenet --dataset wbc3_1024x1024 --phi 0 --gpu 2 --batch-size 8 --steps 100 --epoch 200
-python3 train_wbc.py --docker --snapshot imagenet --dataset wbc3_1024x1024 --phi 0 --gpu 2 --batch-size 8 --steps 100 --epoch 200 --weighted-bifpn
+# python3 train_wbc.py --docker --snapshot imagenet --dataset wbc3_1024x1024 --phi 0 --gpu 2 --batch-size 8 --steps 100 --epoch 200
+# python3 train_wbc.py --docker --snapshot imagenet --dataset wbc3_1024x1024 --phi 0 --gpu 2 --batch-size 8 --steps 100 --epoch 200 --weighted-bifpn
+
+python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --valid False --phi 0 --gpu 0 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 1
+python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --valid False --phi 0 --gpu 0 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 2
+python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --valid False --phi 0 --gpu 0 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 3
