6cd8bba6b7f39aa714efd571156776b745587c60
shenghh2015 <shenghh2015@gmail.com>
2021-08-15 16:36:24 -0500

    shh

--- EfficientDet/predator2.sh
+++ EfficientDet/predator2.sh
@@ -14,5 +14,8 @@
 # python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --valid True --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 2 --lr 1e-4 --lw 0.5
 # python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --valid True --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 3 --lr 1e-4 --lw 0.5
 
-python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc_1024x1024 --valid True --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 1 --lr 5e-5 --lw 0.5 --nb 350
-python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc_1024x1024 --valid True --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 2 --lr 5e-5 --lw 0.5 --nb 350
+# python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc_1024x1024 --valid True --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 1 --lr 5e-5 --lw 0.5 --nb 350
+# python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc_1024x1024 --valid True --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 2 --lr 5e-5 --lw 0.5 --nb 350
+
+python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --valid True --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 2 --lr 5e-5 --lw 0.5 --nb 350
+python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc2_1024x1024 --valid True --phi 0 --gpu 1 --batch-size 8 --steps 100 --epoch 200 --aug False --bstrp 3 --lr 5e-5 --lw 0.5 --nb 350
