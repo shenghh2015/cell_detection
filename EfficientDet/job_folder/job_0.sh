94e94c996165dbc0aa4402b0dc1e1cfbe6be4d0c
shenghh <shenghh2015@gmail.com>
2021-08-18 01:42:39 -0500

    shh

--- EfficientDet/job_folder/job_0.sh
+++ EfficientDet/job_folder/job_0.sh
@@ -1,2 +1,2 @@
-python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc5_1024x1024 --valid False --phi 0 --gpu 0 --batch-size 16 --steps 100 --epoch 200 --aug False --bstrp 0 --lr 5e-5 --lw 0.5 --nb 400
+python3 train_wbc_bootstrap.py --docker --snapshot imagenet --dataset wbc5_1024x1024 --valid False --phi 0 --gpu 0 --batch-size 16 --steps 100 --epoch 200 --aug False --bstrp 4 --lr 5e-5 --lw 0.5 --nb 400
 
